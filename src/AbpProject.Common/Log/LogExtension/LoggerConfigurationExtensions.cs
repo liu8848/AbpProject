1e72be74bf63ac4c0edae942bcd4a4cdaed2eb24
liu.qingzong <sudalu929@outlook.com>
2024-01-10 22:41:31 +0800

    修改日志组件

--- src/AbpProject.Common/Log/LogExtension/LoggerConfigurationExtensions.cs
+++ src/AbpProject.Common/Log/LogExtension/LoggerConfigurationExtensions.cs
@@ -1,6 +1,8 @@
+using AbpProject.Common.Helpers.LogHelpers;
 using Serilog;
 using Serilog.Context;
 using Serilog.Events;
+using Serilog.Filters;
 
 namespace AbpProject.Common.Log.LogExtension;
 
@@ -9,17 +11,28 @@ public static class LoggerConfigurationExtensions
     public static LoggerConfiguration WriteToConsole(this LoggerConfiguration loggerConfiguration)
     {
         //输出普通日志
-        // loggerConfiguration = loggerConfiguration.WriteTo.Logger(lg =>
-        //     lg.FilterRemoveSqlLog().WriteTo.Console());
-        
+        loggerConfiguration.WriteTo.Logger(lg =>
+             lg.FilterRemoveSqlLog().WriteTo.Console(
+                 outputTemplate:LogContextStatic.FileMessageTemplate));
+        //输出SQL
+        // loggerConfiguration.WriteTo.Logger(lg =>
+        // {
+        //     lg.FilterSqlLog()
+        //         .Filter.ByIncludingOnly(Matching
+        //             .WithProperty<bool>(LogContextStatic.SqlOutToConsole, s => s))
+        //         .WriteTo.Console();
+        // });
         return loggerConfiguration;
     }
 
-    // public static LoggerConfiguration FilterRemoveSqlLog(this LoggerConfiguration lc)
-    // {
-    //     lc = lc.Filter.ByIncludingOnly(WithProperty<string>(LogContextStatic.LogSource, s => !LogContextStatic.AopSql.Equals(s)));
-    //     return lc;
-    // }
+    public static LoggerConfiguration FilterRemoveSqlLog(this LoggerConfiguration lc)
+    {
+        // lc = lc.Filter
+        //     .ByIncludingOnly(WithProperty<string>(LogContextStatic.LogSource, s => !LogContextStatic.AopSql.Equals(s)));
+
+        lc = lc.Filter.ByExcluding(Matching.FromSource("Microsoft.EntityFrameworkCore.Database.Command"));
+        return lc;
+    }
 
     public static Func<LogEvent, bool> WithProperty<T>(string propertyName, Func<T, bool> predicate)
     {
@@ -29,5 +42,14 @@ public static class LoggerConfigurationExtensions
 
             return propertyValue is ScalarValue { Value: T value } && predicate(value);
         };
+    }
+
+    public static LoggerConfiguration FilterSqlLog(this LoggerConfiguration lc)
+    {
+        lc = lc.Filter
+            .ByIncludingOnly(Matching
+                .WithProperty<string>(LogContextStatic.LogSource,
+                    s => LogContextStatic.AopSql.Equals(s)));
+        return lc;
     }
 }
